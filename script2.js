9f9b2cf1977c6d5fdf24f9570e7ed57fdd9fa838
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-11-05 21:37:58 -0500

    Got the password generator to generate a password if the user wants all of the conditions met/wants their password to have all of the criteria.

--- script2.js
+++ script2.js
@@ -26,9 +26,8 @@ function getPasswordOptions() {
     hasUppercase: hasUppercaseLetters,
     hasSpecials: hasSpecialCharacters,
   };
-  // console.log(pwdOptions);
-  // return pwdOptions;
-// function GeneratePasswordProcess(){
+  console.log(pwdOptions);
+
   
     var numbers= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     var lower= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -43,84 +42,22 @@ function getPasswordOptions() {
       ) {
         var allArray = numbers.concat(lower, upper, special);
         var passwordString = "";
-        var generatePassword = Math.floor(Math.random() * allArray.length); 
-        for( var i = 0; i < parseInt(passwordLength); i++){
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = allArray[Math.floor(Math.random() * allArray.length)]
           var passwordString = generatePassword + passwordString;
         }
-        // var passwordString = passwordString + generatePassword;
+        
         alert(passwordString);
+
+        console.log(passwordString);
+        console.log(pwdOptions.passwordLength);
       }
   };
 
 
-  // function GeneratePasswordProcess(){
-    // var pwdOptions = {
-    //   passwordLength: passwordLength,
-    //   hasNums: hasNumbers,
-    //   hasLowercase: hasLowercaseLetters,
-    //   hasUppercase: hasUppercaseLetters,
-    //   hasSpecials: hasSpecialCharacters,
-    // }
-    // Object with the options as arrays
-    // var options = {
-    //   numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    //   lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    //   upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    //   special: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-    // };
-  
-    
-    // if(hasNumbers === true &&
-    //   hasLowercaseLetters === true &&
-    //   hasUppercaseLetters === true &&
-    //   hasSpecialCharacters === true
-    //   ) {
-    //     var allArray = options.numbers.concat(options.lower, options.upper, options.special);
-    //     var generatePassword = Math.floor(Math.random(allArray)) * passwordLength; 
-    //     var passwordString = "";
-    //     var customPassword = generatePassword + passwordString;
-    //     alert("Your new password is: " + customPassword);  
-    //   }
-  
-  // }
-  
-  // GeneratePasswordProcess();
-  
-// }
+
 
 startProcessBtn.addEventListener("click", getPasswordOptions);
 
 
-  
-// function GeneratePasswordProcess(){
-//   var pwdOptions = {
-//     passwordLength: passwordLength,
-//     hasNums: hasNumbers,
-//     hasLowercase: hasLowercaseLetters,
-//     hasUppercase: hasUppercaseLetters,
-//     hasSpecials: hasSpecialCharacters,
-//   }
-//   // Object with the options as arrays
-//   var options = {
-//     numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-//     lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-//     upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-//     special: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-//   };
 
-  
-//   if(hasNumbers === true &&
-//     hasLowercaseLetters === true &&
-//     hasUppercaseLetters === true &&
-//     hasSpecialCharacters === true
-//     ) {
-//       var allArray = options.numbers.concat(options.lower, options.upper, options.special);
-//       var generatePassword = Math.floor(Math.random(allArray)) * passwordLength; 
-//       var passwordString = "";
-//       var customPassword = generatePassword + passwordString;
-//       alert("Your new password is: " + customPassword);  
-//     }
-
-// }
-
-// GeneratePasswordProcess();
