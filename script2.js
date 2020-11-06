ed37821a454ae4584cbdffe2548b72d929e14c69
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-11-06 02:42:59 -0500

    Figured out how to get the generated password to replace the placeholder text.

--- script2.js
+++ script2.js
@@ -45,21 +45,51 @@ function getPasswordOptions() {
         for(var i = 0; i <pwdOptions.passwordLength; i++){
           var generatePassword = allArray[Math.floor(Math.random() * allArray.length)]
           var passwordString = generatePassword + passwordString;
-        // document.getElementById("#password").text = passwordString;
-        // console.log(passwordString);
-        // console.log(pwdOptions.passwordLength);
       }
 //If user wants lowers, uppers, and specials
     }else if(hasNumbers === false &&
         hasLowercaseLetters === true &&
         hasUppercaseLetters === true &&
         hasSpecialCharacters === true){
-        var noNumsArray = lower.concat(upper, special);
+        var LUSArray = lower.concat(upper, special);
         var passwordString = "";
         for(var i = 0; i <pwdOptions.passwordLength; i++){
-          var generatePassword = noNumsArray[Math.floor(Math.random() * noNumsArray.length)]
+          var generatePassword = LUSArray[Math.floor(Math.random() * LUSArray.length)]
           var passwordString = generatePassword + passwordString;
         }
+//If user wants numbers, lowers, and specials
+    }else if(hasNumbers === true &&
+      hasLowercaseLetters === true &&
+      hasUppercaseLetters === false &&
+      hasSpecialCharacters === true){
+      var NLSArray = lower.concat(numbers, special);
+      var passwordString = "";
+      for(var i = 0; i <pwdOptions.passwordLength; i++){
+        var generatePassword = NLSArray[Math.floor(Math.random() * NLSArray.length)]
+        var passwordString = generatePassword + passwordString;
+  }
+//If user wants specials, uppers, and numbers
+    }else if(hasNumbers === true &&
+      hasLowercaseLetters === false &&
+      hasUppercaseLetters === true &&
+      hasSpecialCharacters === true){
+      var SUNArray = upper.concat(numbers, special);
+      var passwordString = "";
+      for(var i = 0; i <pwdOptions.passwordLength; i++){
+        var generatePassword = SUNArray[Math.floor(Math.random() * SUNArray.length)]
+        var passwordString = generatePassword + passwordString;
+}
+//If user wants numbers, lowers, and uppers
+}else if(hasNumbers === true &&
+  hasLowercaseLetters === true &&
+  hasUppercaseLetters === true &&
+  hasSpecialCharacters === false){
+  var NLUArray = lower.concat(numbers, upper);
+  var passwordString = "";
+  for(var i = 0; i <pwdOptions.passwordLength; i++){
+    var generatePassword = NLUArray[Math.floor(Math.random() * NLUArray.length)]
+    var passwordString = generatePassword + passwordString;
+}
 //If user wants uppers and special
     }else if(hasNumbers === false &&
         hasLowercaseLetters === false &&
@@ -69,6 +99,72 @@ function getPasswordOptions() {
         var passwordString = "";
         for(var i = 0; i <pwdOptions.passwordLength; i++){
           var generatePassword = noNumsOrLowersArray[Math.floor(Math.random() * noNumsOrLowersArray.length)]
+          var passwordString = generatePassword + passwordString;
+      }
+//If user wants uppers and numbers
+    }else if(hasNumbers === true &&
+        hasLowercaseLetters === false &&
+        hasUppercaseLetters === true &&
+        hasSpecialCharacters === false){
+        var UNArray = upper.concat(numbers);
+        var passwordString = "";
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = UNArray[Math.floor(Math.random() * UNArray.length)]
+          var passwordString = generatePassword + passwordString;
+      }
+//If user wants special and numbers
+    }else if(hasNumbers === true &&
+        hasLowercaseLetters === false &&
+        hasUppercaseLetters === false &&
+        hasSpecialCharacters === true){
+        var SNArray = special.concat(numbers);
+        var passwordString = "";
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = SNArray[Math.floor(Math.random() * SNArray.length)]
+          var passwordString = generatePassword + passwordString;
+      }
+//If user wants uppers and lowers
+    }else if(hasNumbers === false &&
+        hasLowercaseLetters === true &&
+        hasUppercaseLetters === true &&
+        hasSpecialCharacters === false){
+        var ULArray = lower.concat(upper);
+        var passwordString = "";
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = ULArray[Math.floor(Math.random() * ULArray.length)]
+          var passwordString = generatePassword + passwordString;
+      }
+//If user wants specials and lowers
+    }else if(hasNumbers === false &&
+        hasLowercaseLetters === true &&
+        hasUppercaseLetters === false &&
+        hasSpecialCharacters === true){
+        var SLArray = lower.concat(special);
+        var passwordString = "";
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = SLArray[Math.floor(Math.random() * SLArray.length)]
+          var passwordString = generatePassword + passwordString;
+      }
+//If user wants lowers and numbers
+    }else if(hasNumbers === true &&
+        hasLowercaseLetters === true &&
+        hasUppercaseLetters === false &&
+        hasSpecialCharacters === false){
+        var LNArray = lower.concat(numbers);
+        var passwordString = "";
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = LNArray[Math.floor(Math.random() * LNArray.length)]
+          var passwordString = generatePassword + passwordString;
+      }
+//If user wants uppers and numbers
+    }else if(hasNumbers === true &&
+        hasLowercaseLetters === false &&
+        hasUppercaseLetters === true &&
+        hasSpecialCharacters === false){
+        var UNArray = upper.concat(numbers);
+        var passwordString = "";
+        for(var i = 0; i <pwdOptions.passwordLength; i++){
+          var generatePassword = UNArray[Math.floor(Math.random() * UNArray.length)]
           var passwordString = generatePassword + passwordString;
       }
 //If user wants just special
@@ -115,8 +211,12 @@ function getPasswordOptions() {
           var generatePassword = upper[Math.floor(Math.random() * upper.length)]
           var passwordString = generatePassword + passwordString;
   }
+//If user picks no criteria
+    }else{
+      alert("You must pick at least ONE password criteria.");
     }
   alert(passwordString);
+  var passwordEl = document.getElementById("password").placeholder = passwordString;
 };
 
 
